7e9b9a7b6bd62e1e56a8cada72b1f7dcea65decc
Lucas Fernandez Nicolau <dev.lucasfn@gmail.com>
2020-03-12 14:01:29 -0300

    Remoção itens main

--- ReplyChallenge/main.swift
+++ ReplyChallenge/main.swift
@@ -8,30 +8,6 @@
 
 import Foundation
 
-let d1 = Dev(id: 0, company: "mack", bonus: 7, skills: [
-    "Java",
-    "Python",
-    "C"
-])
-
-let d2 = Dev(id: 1, company: "mack", bonus: 4, skills: [
-    "C",
-    "Java",
-    "C++"
-])
-
-let d3 = Dev(id: 1, company: "mack", bonus: 8, skills: [
-    "AWS",
-    "C#",
-    "Brainfuck"
-])
-
-let m1 = Replyer(id: 2, company: "mack", bonus: 10)
-
-print("WP: \(d1.workPotential(for: d2))")
-print("B : \(d1.bonus(for: d2))")
-print("TP: \(d1.totalScore(for: d2))")
-
 //MARK: READ FILE
 let pathURL = URL(fileURLWithPath: (NSString(string:"~/Desktop/_.txt").expandingTildeInPath ))
 let s = StreamReader(url: pathURL)
